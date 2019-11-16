f7468afb27a86a8d5444a4ea84fc5eeb3495a832
Artur Kuzmich <Artur1Kuzmich@gmail.com>
2019-11-16 13:14:50 +0200

    new

--- new folder/index.js
+++ new folder/index.js
@@ -1,10 +1,111 @@
+//const arr = Array(100).fill('');
 
-function sum(callbackFunction) {
-    const res = a + b;
+//function randomNum(){
+    //return Math.round( Math.random() * 255 );
+//}
 
-    if  ( typeof callbackFunction === `function`){
-    callbackFunction(3);
+
+   // function render (selector, list){
+       // const app = document.querySelector(selector);
+
+       // const html = arr.map(e => `
+      //  <div
+       ///=     style="
+         //       width: 50px;
+          //      height: 50px;
+         //      background: rgb(
+          ///         ${randomNum()},
+          ///         ${randomNum()},
+           //         ${randomNum()}
+                  //  )
+      //  "
+       // ></div>
+       // `).join('');
+
+       // app.innerHTML = html;
+
+
+   // }
+//render('#app', arr);
+
+
+
+
+    //function example(){
+    //    let variable = 123;
+
+    //    return function() {
+    //        //console.log(variable)
+    //    }
+   // }
+
+    //let res = example();
+    //console.dir(res);
+
+
+//const constr = q => b => a + b;
+//const sumWith2 = constr(2);
+//console.dir(sumWith2)
+
+//const sumWith5 = constr(5);
+//console.dir(sumWith5)
+//sum(2)(3)//5
+
+
+//const fraseConstructor = frase => name => `${frase}, => ${name}!`
+
+
+//const sayHello = fraseConstructor('Hello');
+//const satGoodbye = fraseConstructor('Goodbye');
+
+//console.log(sayHello('Ivan'))
+//console.log(satGoodbye ('Petro'))
+
+
+//function circleSqueareConstructor(){
+
+  //  const PI = 3.14;
+
+//return radius => PI * R * R;
+
+//}
+
+//const circleSqueare = circleSqueareConstructor();
+
+
+
+const random = () =>  Math.round(Math.random() * 255);
+
+
+let timer = setInterval(render, 500);
+
+
+function render(selector = '#app') {
+    let html = '';
+
+    for(let i = 0; i < 500; i++){
+        html += `
+        <div
+            style="
+            width: 50px;
+            height: 50px;
+            background: rgb(${random()}, ${random()}, ${random()});
+        "
+        ></div>
+
+        `;
+    }
+
+    const element = document.querySelector(selector);
+
+    if (element){
+        element.innerHTML = html;
+
+    }
+        else {
+            console.log('no element')
+            clearInterval(timer);
+        }
 }
-}
 
-sum(e => e === 3)
+ 
