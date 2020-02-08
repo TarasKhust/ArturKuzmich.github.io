cc5a5dae0eb5536c7494844d86202ae6bb91a6bc
Artur Kuzmich <Artur1Kuzmich@gmail.com>
2020-02-08 18:21:39 +0200

    msg

--- Progects/ToDoList/toDoList2.js
+++ Progects/ToDoList/toDoList2.js
@@ -1,28 +1,28 @@
 const list = document.querySelector('ul');
 
-        list.addEventListener('click', function (ev) {
-    if(ev.target.tagName === "LI") {
-       ev.target.classList.toggle('checked');
-    } else if(ev.target.tagName === "SPAN") {
-       const div = ev.target.parentNode;
-       div.remove();
-    }
+list.addEventListener('click', function (ev) {
+   if (ev.target.tagName === "LI") {
+      ev.target.classList.toggle('checked');
+   } else if (ev.target.tagName === "SPAN") {
+      const div = ev.target.parentNode;
+      div.remove();
+   }
 }, false);
 
 function newElement() {
-    const li = document.createElement('li');
-    const inputValue = document.getElementById('toDoEl').value;
-    const t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if(inputValue == "") {
-       alert("Add your item...!");
-    } else {
-       document.getElementById('list').appendChild(li);
-    }
-    document.getElementById('toDoEl').value = "";
-    const span = document.createElement('SPAN');
-    const txt = document.createTextNode("X");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
+   const li = document.createElement('li');
+   const inputValue = document.getElementById('toDoEl').value;
+   const t = document.createTextNode(inputValue);
+   li.appendChild(t);
+   if (inputValue == "") {
+      alert("Add your item...!");
+   } else {
+      document.getElementById('list').appendChild(li);
+   }
+   document.getElementById('toDoEl').value = "";
+   const span = document.createElement('SPAN');
+   const txt = document.createTextNode("X");
+   span.className = "close";
+   span.appendChild(txt);
+   li.appendChild(span);
 }
